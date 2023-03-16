cd8de7c7b366a352861236b2ffd082f1189b4e90
Enzo Rodrigues Olveira <en.zoo@hotmail.com>
2023-03-16 17:19:55 -0300

    adicionando a barra de busca e configurando layout.

--- Front-end/script.js
+++ Front-end/script.js
@@ -58,21 +58,98 @@ jQuery(document).ready(function($){
 
 
 
+				// Search menu //
 
-// Add active class on another page linked
-// ==========================================
-// $(window).on('load',function () {
-//     var current = location.pathname;
-//     console.log(current);
-//     $('#navbarSupportedContent ul li a').each(function(){
-//         var $this = $(this);
-//         // if the current path is like this link, make it active
-//         if($this.attr('href').indexOf(current) !== -1){
-//             $this.parent().addClass('active');
-//             $this.parents('.menu-submenu').addClass('show-dropdown');
-//             $this.parents('.menu-submenu').parent().addClass('active');
-//         }else{
-//             $this.parent().removeClass('active');
-//         }
-//     })
-// });
+/* Search bar */
+
+var resizeElements;
+
+$(document).ready(function() {
+
+  // Set up common variables
+  // --------------------------------------------------
+
+  var bar = ".search_bar";
+  var input = bar + " input[type='text']";
+  var button = bar + " button[type='submit']";
+  var dropdown = bar + " .search_dropdown";
+  var dropdownLabel = dropdown + " > span";
+  var dropdownList = dropdown + " ul";
+  var dropdownListItems = dropdownList + " li";
+
+
+  // Set up common functions
+  // --------------------------------------------------
+
+  resizeElements = function() {
+    var barWidth = $(bar).outerWidth();
+
+    var labelWidth = $(dropdownLabel).outerWidth();
+    $(dropdown).width(labelWidth);
+
+    var dropdownWidth = $(dropdown).outerWidth();
+    var buttonWidth	= $(button).outerWidth();
+    var inputWidth = barWidth - dropdownWidth - buttonWidth;
+    var inputWidthPercent = inputWidth / barWidth * 100 + "%";
+
+    $(input).css({ 'margin-left': dropdownWidth, 'width': inputWidthPercent });
+  }
+
+  function dropdownOn() {
+    $(dropdownList).fadeIn(25);
+    $(dropdown).addClass("active");
+  }
+
+  function dropdownOff() {
+    $(dropdownList).fadeOut(25);
+    $(dropdown).removeClass("active");
+  }
+
+
+  // Initialize initial resize of initial elements 
+  // --------------------------------------------------
+  resizeElements();
+
+
+  // Toggle new dropdown menu on click
+  // --------------------------------------------------
+
+  $(dropdown).click(function(event) {
+    if ($(dropdown).hasClass("active")) {
+      dropdownOff();
+    } else {
+      dropdownOn();
+    }
+
+    event.stopPropagation();
+    return false;
+  });
+
+  $("html").click(dropdownOff);
+
+
+  // Activate new dropdown option and show tray if applicable
+  // --------------------------------------------------
+
+  $(dropdownListItems).click(function() {
+    $(this).siblings("li.selected").removeClass("selected");
+    $(this).addClass("selected");
+
+    // Focus the input
+    $(this).parents("form.search_bar:first").find("input[type=text]").focus();
+
+    var labelText = $(this).text();
+    $(dropdownLabel).text(labelText);
+
+    resizeElements();
+
+  });
+
+
+  // Resize all elements when the window resizes
+  // --------------------------------------------------
+
+  $(window).resize(function() {
+    resizeElements();
+  });
+});
